ea3763512e224513841a28775ed5b0f8298871eb
OpencvZLG <1019383856@qq.com>
2023-09-21 11:14:58 +0800

    add cmd params to generate cert example

--- cmd/generateCert/main.go
+++ cmd/generateCert/main.go
@@ -10,9 +10,22 @@
 
 package main
 
-import . "ginFlutterBolg/util"
+import (
+	"flag"
+	. "ginFlutterBolg/util"
+)
 
 func main() {
-	GenerateCert("cert", "www.cilang.buzz", "CN",
-		"GuangZhou", "GuangZhou", "cilang.buzz", "cilang", "localhost")
+	fileType := flag.String("fileType", "cert", "Save the format")
+	organization := flag.String("organization", "cilangOrganization", "cert organization")
+	country := flag.String("country", "CN", "province Country")
+	province := flag.String("province", "CN", "cert province")
+	locality := flag.String("locality", "GuangZhou", "locality")
+	organizationUnit := flag.String("organizationUnit", "cilang.buzz", "organizationUnit")
+	commonName := flag.String("commonName", "cilang", "commonName")
+	dnsDomain := flag.String("dnsDomain", "localhost", "dnsDomain")
+	flag.Parse()
+	GenerateCert(*fileType, *organization, *country, *province, *locality, *organizationUnit, *commonName, *dnsDomain)
+	//GenerateCert("cert", "www.cilang.buzz", "CN",
+	//	"GuangZhou", "GuangZhou", "cilang.buzz", "cilang", "localhost")
 }
